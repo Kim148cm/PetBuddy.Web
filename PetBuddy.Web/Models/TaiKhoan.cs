ef8e07e9a375a1f1144e4e9cb30fe392e7d504a1
Kim148cm <thienkymz@gmail.com>
2026-02-07 20:04:42 +0700

    Add project files.

---
+++ PetBuddy.Web/Models/TaiKhoan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetBuddy.Web.Models
+{
+    public class TaiKhoan
+    {
+        public int MaTaiKhoan { get; set; }
+        public string Email { get; set; }
+        public string MatKhau { get; set; }
+        public string HoTen { get; set; }
+        public string SoDienThoai { get; set; }
+        public string DiaChi { get; set; }
+        public string VaiTro { get; set; }
+        public bool TrangThai { get; set; }
+        public DateTime NgayTao { get; set; }
+    }
+}
